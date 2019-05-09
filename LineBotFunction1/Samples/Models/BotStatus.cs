682d6ff00c1415c82ece93c11672c861f9ba9984
ymgw0867 <kyamagiwa@gmail.com>
2019-05-09 15:44:57 +0900

    プロジェクト ファイルを追加します。

---
+++ LineBotFunction1/Samples/Models/BotStatus.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+
+namespace LineBotFunction1
+{
+    public class BotStatus : EventSourceState
+    {
+        public string Location { get; set; }
+        public string CurrentApp { get; set; }
+
+        public BotStatus() { }
+    }
+}
---
+++ LineBotFunction1/Samples/Models/EventSourceState.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+
+namespace LineBotFunction1
+{
+    public class EventSourceState : TableEntity
+    {
+        [IgnoreProperty]
+        public string SourceType { get => PartitionKey; set => PartitionKey = value; }
+        [IgnoreProperty]
+        public string SourceId { get => RowKey; set => RowKey = value; }
+
+        public EventSourceState() { }
+    }
+}
